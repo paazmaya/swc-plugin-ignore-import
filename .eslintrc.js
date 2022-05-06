06fa4ef50009f512db4abd6ef1976b1763e931b2
Juga Paazmaya <paazmaya@yahoo.com>
2022-05-06 22:17:56 +0300

    Use a JavaScript file for configuring ESLint, to allow comments

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['paazmaya', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'func-style': 'off'
+  }
+};
