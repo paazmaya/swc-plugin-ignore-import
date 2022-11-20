9097044675bcdb06ab3128de1feca2a5b973343a
Juga Paazmaya <paazmaya@yahoo.com>
2022-11-20 17:27:50 +0200

    Migrating ESLint to its new flat config style

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import paazmaya from 'eslint-config-paazmaya';
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  paazmaya,
+  {
+    plugins: {
+      tsPlugin
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      'func-style': 'off'
+    }
+  }
+];
